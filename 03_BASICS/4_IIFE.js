2d726f3898b722a778f21df8a8c563aef674655d
Jigisha Singh <rim2004jhim@gmail.com>
2024-07-27 23:54:24 +0530

    how does js execute code

--- 03_BASICS/4_IIFE.js
+++ 03_BASICS/4_IIFE.js
@@ -18,3 +18,19 @@ and immediately execute
     console.log(`DB CONNECTED TWO ${name}`)
 })('jigisha') 
 //passing an argument 
+
+//javascript is single threaded
+
+/*
+Javascript Execution Context
+1.Global Execution Context(this)
+2.function Execution Context
+3.Eval Execution Context
+
+How does js execute code?
+1.global execution phase
+2.memory creation phase-allocation of space/memory for variables
+3.execution phase-executes the code
+(every time a function is made memory creation phase+execution phase occurs)
+(when the execution part is completed it gets deleted)
+*/
